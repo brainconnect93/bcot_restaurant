2785406548ddaf705a03b477b5080f2f6108c344
brainconnect93 <brainconnect0@gmail.com>
2023-03-24 22:25:31 +0100

    Install react icons for usage

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
-function App() {
-	return (
-		null
-	);
+import React from 'react'
+
+const App = () => {
+  return (
+	<div>
+		<h1>This is my restaurant app</h1>
+	</div>
+  )
 }
 
-export default App;
+export default App
