2a7229515577ec1c0581b537203322e3f23258b3
brainconnect93 <brainconnect0@gmail.com>
2023-04-05 13:49:25 -0700

    Add the media quey for other screen size

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,31 +1,55 @@
-import React from 'react';
-// import { GiHamburgerMenu } from 'react-icons/gi';
-// import { MdOutlineRestaurantMenu } from 'react-icons/md';
+import React, { useState } from 'react';
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { MdOutlineRestaurantMenu } from 'react-icons/md';
 import { gericht } from '../../constants/images';
 
 import './Navbar.css';
 
-const Navbar = () => (
-  <>
-    <nav className="app_navbar">
-      <div className="app_navbar-logo">
-        <img src={gericht} alt="app logo" />
-      </div>
-      <ul className="app_navbar-links">
-        <li className="p_opensans"><a href="#home">Home</a></li>
-        <li className="p_opensans"><a href="#about">About</a></li>
-        <li className="p_opensans"><a href="#menu">Menu</a></li>
-        <li className="p_opensans"><a href="#awards">Awards</a></li>
-        <li className="p_opensans"><a href="#contact">Contact</a></li>
-      </ul>
+const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
 
-      <div className="app_navbar-login">
-        <a href="#login" className="p_opensans">Log In / Register</a>
-        <div />
-        <a href="#book" className="p_opensans">Book Table</a>
-      </div>
-    </nav>
-  </>
-);
+  return (
+    <>
+      <nav className="app_navbar">
+        <div className="app_navbar-logo">
+          <img src={gericht} alt="app logo" />
+        </div>
+        <ul className="app_navbar-links">
+          <li className="p_opensans"><a href="#home">Home</a></li>
+          <li className="p_opensans"><a href="#about">About</a></li>
+          <li className="p_opensans"><a href="#menu">Menu</a></li>
+          <li className="p_opensans"><a href="#awards">Awards</a></li>
+          <li className="p_opensans"><a href="#contact">Contact</a></li>
+        </ul>
+
+        <div className="app_navbar-login">
+          <a href="#login" className="p_opensans">Log In / Register</a>
+          <div />
+          <a href="#book" className="p_opensans">Book Table</a>
+        </div>
+
+        {/* <-- for mobile screen size !--> */}
+
+        <div className="app_navbar-mob">
+          <GiHamburgerMenu color="#fff" fontSize={27} onClick={() => setToggleMenu(true)} />
+
+          {toggleMenu && (
+          <div className="app_navbar-mob_overlay flex_center slide-bottom">
+            <MdOutlineRestaurantMenu fontSize={27} className="overlay_close" onClick={() => setToggleMenu(false)} />
+
+            <ul className="app_navbar-mob-links">
+              <li className="p_opensans"><a href="#home">Home</a></li>
+              <li className="p_opensans"><a href="#about">About</a></li>
+              <li className="p_opensans"><a href="#menu">Menu</a></li>
+              <li className="p_opensans"><a href="#awards">Awards</a></li>
+              <li className="p_opensans"><a href="#contact">Contact</a></li>
+            </ul>
+          </div>
+          )}
+        </div>
+      </nav>
+    </>
+  );
+};
 
 export default Navbar;
