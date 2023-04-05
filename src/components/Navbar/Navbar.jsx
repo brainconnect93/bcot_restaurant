d7eadff0f8338a18bb78d00c26b284e612eeed32
brainconnect93 <brainconnect0@gmail.com>
2023-04-05 00:06:40 -0700

    Start with the navbar for the header section

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,25 @@
 import React from 'react';
+// import { GiHamburgerMenu } from 'react-icons/gi';
+// import { MdOutlineRestaurantMenu } from 'react-icons/md';
+import { gericht } from '../../constants/images';
+
+import './Navbar.css';
 
 const Navbar = () => (
-  <div>Navbar</div>
+  <>
+    <nav className="app_navbar">
+      <div className="app_navbar-logo">
+        <img src={gericht} alt="app logo" />
+      </div>
+      <ul className="app_navbar-links">
+        <li className="p_opensans"><a href="#home">Home</a></li>
+        <li className="p_opensans"><a href="#about">About</a></li>
+        <li className="p_opensans"><a href="#menu">Menu</a></li>
+        <li className="p_opensans"><a href="#awards">Awards</a></li>
+        <li className="p_opensans"><a href="#contact">Contact</a></li>
+      </ul>
+    </nav>
+  </>
 );
 
 export default Navbar;
