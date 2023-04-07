4e662bb89c9f1fa01c5befcf6780a4a87ea765a1
brainconnect93 <brainconnect0@gmail.com>
2023-04-06 23:46:08 -0700

    Start with the about section

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -1,7 +1,34 @@
 import React from 'react';
+import { G, spoon, knife } from '../../constants/images';
+
+import './AboutUs.css';
 
 const AboutUs = () => (
-  <div>AboutUs</div>
+  <div className="app_aboutus app_bg flex_center section_padding" id="about">
+    <div className="app_aboutus_overlay flex_center">
+      <img src={G} alt="g letter" />
+    </div>
+
+    <div className="app_aboutus-content flex_center">
+      <div className="app_about-content_about">
+        <h1 className="headtext_cormorant">About Us</h1>
+        <img src={spoon} alt="about-spoon" className="about_img" />
+        <p className="p_opensans">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis pharetra adipiscing ultrices vulputate posuere tristique. In sed odio nec aliquet eu proin mauris et.</p>
+        <button type="button" className="custom_button">Know More</button>
+      </div>
+
+      <div className="app_aboutus-knife flex_center">
+        <img src={knife} alt="knife" />
+      </div>
+
+      <div className="app_about-content_about">
+        <h1 className="headtext_cormorant">Our History</h1>
+        <img src={spoon} alt="about-spoon" className="about_img" />
+        <p className="p_opensans">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis pharetra adipiscing ultrices vulputate posuere tristique. In sed odio nec aliquet eu proin mauris et.</p>
+        <button type="button" className="custom_button">Know More</button>
+      </div>
+    </div>
+  </div>
 );
 
 export default AboutUs;
