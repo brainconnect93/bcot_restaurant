cd26ec735bfceec88671207acb873cb99fadc24b
brainconnect93 <brainconnect0@gmail.com>
2023-04-06 23:16:45 -0700

    Revert the Gallery file deleted

--- src/container/index.js
+++ src/container/index.js
@@ -2,6 +2,7 @@ import AboutUs from './AboutUs/AboutUs';
 import Chef from './Chef/Chef';
 import Findus from './Findus/Findus';
 import Footer from './Footer/Footer';
+import Gallery from './Gallery/Gallery';
 import Header from './Header/Header';
 import Intro from './Intro/Intro';
 import Laurels from './Laurels/Laurels';
@@ -12,6 +13,7 @@ export {
   Chef,
   Findus,
   Footer,
+  Gallery,
   Header,
   Intro,
   Laurels,
