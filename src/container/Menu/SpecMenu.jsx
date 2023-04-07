492f8ecbccd3ce4ae65ba58bdcb5f235ca36a4c6
brainconnect93 <brainconnect0@gmail.com>
2023-04-07 00:34:28 -0700

    Start the special menu of the page

--- src/container/Menu/SpecMenu.jsx
+++ src/container/Menu/SpecMenu.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
 
+import { SubHeading, MenuItem } from '../../components'
+
+import './SpecMenu.css'
+
 const SpecMenu = () => (
-  <div>SpecMenu</div>
+  <div className="app_specMenu flex_center section_padding" id="menu">
+    <div className="app_specMenu-title">
+      <SubHeading title="Menu that fits you palatte" />
+      <h1 className="headtext_cormorant">Today’s Special</h1>
+    </div>
+  </div>
 );
 
 export default SpecMenu;
