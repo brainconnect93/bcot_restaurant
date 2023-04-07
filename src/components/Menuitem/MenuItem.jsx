14c198848761710d2b1a8f5c4e4b180896b04535
brainconnect93 <brainconnect0@gmail.com>
2023-04-07 11:55:36 -0700

    Add the menuitem as a re-usable component

--- src/components/Menuitem/MenuItem.jsx
+++ src/components/Menuitem/MenuItem.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
 
-const MenuItem = () => (
-  <div>MenuItem</div>
+const MenuItem = ({ title, price, tags }) => (
+  <div className="app_menuitem">
+    <div className="app_menuitem-head">
+      <div className="app_menuitem-name">
+        <p className="p_cormorant" style={{ color: '#dcca87' }}>{title}</p>
+      </div>
+
+      <div className="app_menuitem-dash" />
+
+      <div className="app_menuitem-price">
+        <p className="p_cormorant">{price}</p>
+      </div>
+    </div>
+
+    <div className="app_menuitem-sub">
+      <p className="p_opensans" style={{ color: '#aaa' }}>{tags}</p>
+    </div>
+  </div>
 );
 
 export default MenuItem;
