5f3dfbd415b7e8fd46b58f1a4057d379621bd4c4
brainconnect93 <brainconnect0@gmail.com>
2023-04-08 12:40:15 +0100

    Start the chef section of the website

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import './Chef.css';
+import { chef } from '../../constants/images';
+import { SubHeading } from '../../components';
 
 const Chef = () => (
-  <div>Chef</div>
+  <div className="app_bg app_wrapper section_padding">
+    <div className="app_wrapper-img app_wrapper-img-reverse">
+      <img src={chef} alt="chef" />
+    </div>
+
+    <div className="app_wrapper-info">
+      <SubHeading title="Chef's Word" />
+      <h1 className="headtext_cormorant">What We Believe In</h1>
+    </div>
+  </div>
 );
 
 export default Chef;
