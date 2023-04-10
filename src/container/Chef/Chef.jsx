2927267936c1725523543ad2b861efa58cb2b87d
brainconnect93 <brainconnect0@gmail.com>
2023-04-10 12:01:57 +0100

    Start the intro section of the page

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -13,19 +13,19 @@ const Chef = () => (
       <SubHeading title="Chef's Word" />
       <h1 className="headtext_cormorant">What We Believe In</h1>
 
-    <div className="app_chef-content">
-      <div className="app_chef-quote">
-        <img src={quote} alt="quote" />
-        <p className="p_opensans">Lorem ipsum dolor sit amet, consectetur adipiscing elit auctor sit.</p>
+      <div className="app_chef-content">
+        <div className="app_chef-quote">
+          <img src={quote} alt="quote" />
+          <p className="p_opensans">Lorem ipsum dolor sit amet, consectetur adipiscing elit auctor sit.</p>
+        </div>
+        <p className="p_opensans">auctor sit iaculis in arcu. Vulputate nulla lobortis mauris eget sit. Nulla scelerisque scelerisque congue ac consequat, aliquam molestie lectus eu. Congue iaculis integer curabitur semper sit nunc.</p>
       </div>
-      <p className="p_opensans">auctor sit iaculis in arcu. Vulputate nulla lobortis mauris eget sit. Nulla scelerisque scelerisque congue ac consequat, aliquam molestie lectus eu. Congue iaculis integer curabitur semper sit nunc.</p>
-    </div>
 
-    <div className="app_chef-sign">
-      <p>Kevin Luo</p>
-      <p className="p_opensans">Chef & Founder</p>
-      <img src={sign} alt="Signature" />
-    </div>
+      <div className="app_chef-sign">
+        <p>Kevin Luo</p>
+        <p className="p_opensans">Chef & Founder</p>
+        <img src={sign} alt="Signature" />
+      </div>
     </div>
   </div>
 );
--- src/container/Intro/Intro.jsx
+++ src/container/Intro/Intro.jsx
@@ -1,7 +1,49 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
+import { BsFillPlayFill, BsPauseFill } from 'react-icons/bs';
+import { meal } from '../../constants';
+import './Intro.css';
 
-const Intro = () => (
-  <div>Intro</div>
-);
+const Intro = () => {
+  const [playVideo, setplayVideo] = useState(false);
+  const vidRef = useRef();
+  const handleVideo = () => {
+    setplayVideo((prevPlayVideo) => !prevPlayVideo);
+
+    if (playVideo) {
+      vidRef.current.pause();
+    } else {
+      vidRef.current.play();
+    }
+  };
+
+  return (
+    <div className="app_video">
+      <video
+        src={meal}
+        ref={vidRef}
+        type="video/mp4"
+        loop
+        controls={false}
+        muted
+      />
+
+      <div className="app_video-overlay flex_center">
+        <div
+          className="app_video-overlay-circle flex_center"
+          role="button"
+          tabIndex={0}
+          onClick={handleVideo}
+          onKeyDown={(event) => {
+            if (event.key === 'Enter' || event.key === ' ') {
+              // Trigger the same action as the click event handle
+            }
+          }}
+        >
+          {playVideo ? <BsPauseFill color="#fff" fontSize={30} /> : <BsFillPlayFill color="#fff" fontSize={30} />}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Intro;
