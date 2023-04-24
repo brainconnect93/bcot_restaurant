79a5f7cd5b288856c07fe0127a9755980f332270
brainconnect93 <brainconnect0@gmail.com>
2023-04-25 00:44:48 +0100

    Starting the gallery section

--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import { BsInstagram, BsArrowLeftShort, BsArrowRightShort } from 'react-icons/bs';
 
-const Gallery = () => (
-  <div>Gallery</div>
-);
+import { SubHeading } from '../../components';
+import { data } from '../../constants';
+import './Gallery.css';
+
+const Gallery = () => {
+  return (
+    <div className="app_gallery flec_center">
+      <div className="app_gallery-content">
+        <SubHeading title="Instagram" />
+      </div>
+    </div>
+  );
+}
 
 export default Gallery;
