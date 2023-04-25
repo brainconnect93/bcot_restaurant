d2a8fd0cdafff5978e4af82045e1d8e9a703d0dc
brainconnect93 <brainconnect0@gmail.com>
2023-04-25 23:23:37 +0100

    start making the footer scetion functionality

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import {
+  FiFacebook, FiTwitter, FiInstagram, FiYoutube,
+} from 'react-icons/fi';
+import { FooterOverlay, Newsletter } from '../../components';
+
+import './Footer.css';
 
 const Footer = () => (
-  <div>Footer</div>
+  <div className="app_footer section_padding">
+    <FooterOverlay />
+    <Newsletter />
+  </div>
 );
 
 export default Footer;
