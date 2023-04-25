6394527ef02db058ea12e5ab16a7052decb7f91d
brainconnect93 <brainconnect0@gmail.com>
2023-04-25 01:39:53 +0100

    Start styling for a desktop version

--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -2,11 +2,25 @@ import React from 'react';
 import { BsInstagram, BsArrowLeftShort, BsArrowRightShort } from 'react-icons/bs';
 
 import { SubHeading } from '../../components';
-import { data } from '../../constants';
+import {
+  gallery01, gallery02, gallery03, gallery04,
+} from '../../constants/images';
 import './Gallery.css';
 
+const images = [gallery01, gallery02, gallery03, gallery04];
+
 const Gallery = () => {
-  const scrollRef = React.useRef(null)
+  const scrollRef = React.useRef(null);
+
+  const scroll = (direction) => {
+    const { current } = scrollRef;
+
+    if (direction === 'left') {
+      current.scrollLeft -= 300;
+    } else {
+      current.scrollLeft += 300;
+    }
+  };
   return (
     <div className="app_gallery flec_center">
       <div className="app_gallery-content">
@@ -17,15 +31,21 @@ const Gallery = () => {
       </div>
 
       <div className="app_gallery-img">
-        <div className="app_gallery-img_container" ref={screenRef}>
-
+        <div className="app_gallery-img_container" ref={scrollRef}>
+          {images.map((image, index) => (
+            <div className="app_gallery-img_card flex_center" key={`gallery_image-${index + 1}`}>
+              <img src={image} alt="gallery" />
+              <BsInstagram className="gallery_image-icon" />
+            </div>
+          ))}
         </div>
         <div className="app_gallery-img_arrow">
-          <BsArrowLeftShort />
+          <BsArrowLeftShort className="gallery_arrow-icon" onClick={() => scroll('left')} />
+          <BsArrowRightShort className="gallery_arrow-icon" onClick={() => scroll('left')} />
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default Gallery;
